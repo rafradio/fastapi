67f157548986acbc5a403a8fc1ec8783c9bbf56e
Rafail Abdyushev <rafaelab@mail.ru>
2023-10-16 14:58:31 +0300

    models

--- models.py
+++ models.py
@@ -1,20 +1,8 @@
-# import sqlalchemy
-# from typing import List
-# from typing import Optional
-# from sqlalchemy.orm import Mapped
-# from sqlalchemy.orm import mapped_column
-# from sqlalchemy.orm import relationship
-# from sqlalchemy.orm import DeclarativeBase
-
-# from sqlalchemy import ForeignKey
-# from sqlalchemy import Integer
-
-from __future__ import annotations
+# from __future__ import annotations
 from typing import List
 from datetime import datetime
-
 from sqlalchemy import ForeignKey
-from sqlalchemy import Integer, String, Text, Float, DateTime
+from sqlalchemy import Integer, String, Text, Float, DateTime, Boolean, Column
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import DeclarativeBase
@@ -26,31 +14,26 @@ class Base(DeclarativeBase):
     pass
 
 # Base = declarative_base()
-# db = sqlalchemy()
 
 class Client(Base):
     __tablename__ = "clients"
-    # __table_args__ = {'schema': 'public'}
-
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     first_name: Mapped[str] = mapped_column(String(80))
     second_name: Mapped[str] = mapped_column(String(80))
     email: Mapped[str] = mapped_column(String(120))
     password: Mapped[str] = mapped_column(String(20))
-    orders: Mapped[List["Order"]] = relationship(back_populates="client")
+    orders: Mapped[List["Order"]] = relationship("Order")
 
 class Merchant(Base):
     __tablename__ = "merchants"
-    # __table_args__ = {'schema': 'public'}
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     title: Mapped[str] = mapped_column(String(120))
     description: Mapped[str] = mapped_column(Text())
     price: Mapped[float] = mapped_column(Float(precision=2))
-    orders: Mapped[List["Order"]] = relationship(back_populates="merchant")
+    orders: Mapped[List["Order"]] = relationship("Order")
     
 class Order(Base):
-    __tablename__ = 'orders'
-    # __table_args__ = {'schema': 'public'}
+    __tablename__ = "orders"
     id: Mapped[int] = mapped_column(primary_key=True)
     client_id: Mapped[int] = mapped_column(ForeignKey("clients.id"))
     merchant_id: Mapped[int] = mapped_column(ForeignKey("merchants.id"))
